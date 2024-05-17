4347d3bac0d7b8e983e258504fefb19b05092475
aafrinshahas <codergit6@gmail.com>
2024-05-17 13:07:05 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Responsive Landing Page
+
+This project is a responsive landing page built using HTML, CSS, and JavaScript. It is designed to adapt and display properly on various devices, including desktops, tablets, and smartphones.
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+
+
+## Demo
+
+Include a link to the live demo of your landing page if available.
+
+## Features
+
+- Responsive design: The landing page is optimized for various screen sizes and devices.
+- Interactive elements: JavaScript is used to add interactive features such as animations, sliders, or modal windows.
+- Custom styling: CSS is used to style the layout and components to enhance the visual appeal of the landing page.
+
+## Technologies Used
+
+- HTML: Used for the structure and content of the landing page.
+- CSS: Used for styling and layout.
+- JavaScript: Used for adding interactive elements and dynamic behavior to the page.
+
+
---
+++ assest/js/index.js
@@ -0,0 +1,98 @@
+
+function myFunction() {
+    var x = document.getElementById("display");
+    var y = document.getElementById("session");
+    var x1 = document.getElementById("display1");
+    var y1 = document.getElementById("session1");
+    var x2 = document.getElementById("display2");
+    var y2 = document.getElementById("session2");
+    var x3 = document.getElementById("display3");
+    var y3 = document.getElementById("session3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.backgroundColor = "#FFCC00"
+      x1.style.display = "none";
+      y1.style.backgroundColor = "#FFF9E1"
+      x2.style.display = "none";
+      y2.style.backgroundColor = "#FFF9E1"
+      x3.style.display = "none";
+      y3.style.backgroundColor = "#FFF9E1"
+    } else {
+      x.style.display = "none";
+      y.style.backgroundColor = "#FFF9E1"
+    }
+  }
+
+  function myFunction1(){
+    var x = document.getElementById("display");
+    var y = document.getElementById("session")
+    var x1= document.getElementById("display1");
+    var y1 = document.getElementById("session1");
+    var x2 = document.getElementById("display2");
+    var y2 = document.getElementById("session2");
+    var x3 = document.getElementById("display3");
+    var y3 = document.getElementById("session3");
+    if(x1.style.display === "none"){
+      x1.style.display = "block";
+      y1.style.backgroundColor = "#FFCC00";
+      x.style.display = "none"
+      y.style.backgroundColor = "#FFF9E1"
+      x2.style.display = "none";
+      y2.style.backgroundColor = "#FFF9E1"
+      x3.style.display = "none";
+      y3.style.backgroundColor = "#FFF9E1"
+    }
+    else{
+      x1.style.display = "none";
+      y1.style.backgroundColor = "#FFF9E1"
+    }
+  }
+
+  function myFunction2(){
+    var x = document.getElementById("display");
+    var y = document.getElementById("session")
+    var x1= document.getElementById("display1");
+    var y1 = document.getElementById("session1");
+    var x2 = document.getElementById("display2");
+    var y2 = document.getElementById("session2");
+    var x3 = document.getElementById("display3");
+    var y3 = document.getElementById("session3");
+    if(x2.style.display === "none"){
+      x2.style.display = "block";
+      y2.style.backgroundColor = "#FFCC00";
+      x.style.display = "none"
+      y.style.backgroundColor = "#FFF9E1"
+      x1.style.display = "none";
+      y1.style.backgroundColor = "#FFF9E1"
+      x3.style.display = "none";
+      y3.style.backgroundColor = "#FFF9E1"
+    }
+    else{
+      x2.style.display = "none";
+      y2.style.backgroundColor = "#FFF9E1"
+    }
+  }
+  function myFunction3(){
+    var x = document.getElementById("display");
+    var y = document.getElementById("session")
+    var x1= document.getElementById("display1");
+    var y1 = document.getElementById("session1");
+    var x2 = document.getElementById("display2");
+    var y2 = document.getElementById("session2");
+    var x3 = document.getElementById("display3");
+    var y3 = document.getElementById("session3");
+    if(x3.style.display === "none"){
+      x3.style.display = "block";
+      y3.style.backgroundColor = "#FFCC00";
+      x.style.display = "none"
+      y.style.backgroundColor = "#FFF9E1"
+      x1.style.display = "none";
+      y1.style.backgroundColor = "#FFF9E1"
+      x2.style.display = "none";
+      y2.style.backgroundColor = "#FFF9E1"
+    }
+    else{
+      x3.style.display = "none";
+      y3.style.backgroundColor = "#FFF9E1"
+    }
+  }
